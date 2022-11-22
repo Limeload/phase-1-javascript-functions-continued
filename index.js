54c36e26b4bbb4c6be21b0462324c6f0beb7e848
Shraddha Rao <shraddharaom@gmail.com>
2022-11-21 21:40:34 -0600

    commit tree eb2800d21d7fec6a8cbe035965a7feaa3fccd1a0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity1 ="roller-skate" , activity2 ="bathe my dog") {
+    return ("This Saturday, I want to " + activity1 + "!");
+}
+
+console.log ("This Saturday, I want to " + activity2 + "!");
+
+function mondayWork(activity1 = "go to the office", activity2 ="work from home"){
+     return ("This Monday, I will " + activity1 + ".");
+}
+console.log ("This Monday, I will " + activity2 + ".");
+
+function wrapAdjective(specialCharacter = "*") {
+      return function inner(adjective = "") {
+    return (`You are ${specialCharacter}${adjective}${specialCharacter}!`)
+   }
+}
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer");
